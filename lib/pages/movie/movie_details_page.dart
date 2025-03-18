0c62b46e537238bfea520d9dc5a52817cb1877cf
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2025-03-17 22:53:11 -0400

    reapires overflow on movie and serie details

--- lib/pages/movie/movie_details_page.dart
+++ lib/pages/movie/movie_details_page.dart
@@ -45,150 +45,154 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
                 automaticallyImplyLeading: false,
                 centerTitle: true,
               ),
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Image.network(
-                      height: 35.h,
-                      '${dotenv.env['IMG_URL']}${movieDetails.posterPath}',
-                      fit: BoxFit.cover,
+              body: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Image.network(
+                        height: 35.h,
+                        '${dotenv.env['IMG_URL']}${movieDetails.posterPath}',
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 2.h),
-                  Center(
-                    child: SizedBox(
-                      width: 70.w,
-                      height: 2.h,
+                    SizedBox(height: 2.h),
+                    Center(
+                      child: SizedBox(
+                        width: 70.w,
+                        height: 2.h,
+                        child:
+                            movieDetails.genres != null
+                                ? ListView.separated(
+                                  itemCount: movieDetails.genres!.length,
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder: (context, index) {
+                                    return Text(
+                                      '${movieDetails.genres![index].name}',
+                                      style: GoogleFonts.lora(
+                                        fontSize: 14.sp,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    );
+                                  },
+                                  separatorBuilder:
+                                      (context, index) => SizedBox(width: 2.w),
+                                )
+                                : SizedBox(),
+                      ),
+                    ),
+                    SizedBox(height: 2.h),
+                    Center(
                       child:
-                          movieDetails.genres != null
-                              ? ListView.separated(
-                                itemCount: movieDetails.genres!.length,
-                                scrollDirection: Axis.horizontal,
-                                itemBuilder: (context, index) {
-                                  return Text(
-                                    '${movieDetails.genres![index].name}',
-                                    style: GoogleFonts.lora(
-                                      fontSize: 14.sp,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  );
-                                },
-                                separatorBuilder:
-                                    (context, index) => SizedBox(width: 2.w),
+                          movieDetails.productionCompanies != null
+                              ? SizedBox(
+                                width: 80.w,
+                                height: 4.h,
+                                child: ListView.separated(
+                                  scrollDirection: Axis.horizontal,
+                                  separatorBuilder:
+                                      (context, index) => SizedBox(width: 2.w),
+                                  itemCount:
+                                      movieDetails.productionCompanies!.length,
+                                  itemBuilder: (context, index) {
+                                    return Image.network(
+                                      '${dotenv.env['IMG_URL']}${movieDetails.productionCompanies![index].logoPath}',
+                                      errorBuilder:
+                                          (context, error, stackTrace) =>
+                                              Icon(Icons.error, size: 4.h),
+                                    );
+                                  },
+                                ),
                               )
                               : SizedBox(),
                     ),
-                  ),
-                  SizedBox(height: 2.h),
-                  Center(
-                    child:
-                        movieDetails.productionCompanies != null
-                            ? SizedBox(
-                              width: 80.w,
-                              height: 4.h,
+                    SizedBox(height: 2.h),
+                    Center(
+                      child:
+                          movieDetails.runtime != null
+                              ? Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Estreno: ${Conversors.convertDateToString(movieDetails.releaseDate!)}',
+                                    style: GoogleFonts.lora(
+                                      fontSize: 12.sp,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  SizedBox(width: 2.w),
+                                  Text(
+                                    'Duración: ${movieDetails.runtime}',
+                                    style: GoogleFonts.lora(
+                                      fontSize: 12.sp,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              )
+                              : Text('Duración: Unkown'),
+                    ),
+                    SizedBox(height: 2.h),
+                    Center(
+                      child:
+                          movieDetails.overview != null
+                              ? Text(
+                                '${movieDetails.overview}',
+                                style: GoogleFonts.lora(
+                                  fontSize: 12.sp,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                textAlign: TextAlign.center,
+                              )
+                              : SizedBox(),
+                    ),
+                    SizedBox(height: 2.h),
+                    Center(
+                      child:
+                          movieDetails.voteAverage != null
+                              ? StarRating(
+                                rating: movieDetails.voteAverage!,
+                                starSize: 22.sp,
+                              )
+                              : Text('Rating: Unkown'),
+                    ),
+                    SizedBox(height: 2.h),
+                    SizedBox(
+                      height: 200,
+                      child: FutureBuilder(
+                        future: MoviesRepository().getMovieCast(widget.movieId),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(child: CircularProgressIndicator());
+                          }
+                          if (snapshot.hasError) {
+                            return Text(snapshot.error.toString());
+                          }
+                          if (snapshot.hasData) {
+                            final cast = snapshot.data!;
+                            return SizedBox(
+                              width: double.infinity,
                               child: ListView.separated(
+                                itemCount: cast.length,
                                 scrollDirection: Axis.horizontal,
+                                itemBuilder: (context, index) {
+                                  return CastCard(cast: cast[index]);
+                                },
                                 separatorBuilder:
                                     (context, index) => SizedBox(width: 2.w),
-                                itemCount:
-                                    movieDetails.productionCompanies!.length,
-                                itemBuilder: (context, index) {
-                                  return Image.network(
-                                    '${dotenv.env['IMG_URL']}${movieDetails.productionCompanies![index].logoPath}',
-                                    errorBuilder:
-                                        (context, error, stackTrace) =>
-                                            Icon(Icons.error, size: 4.h),
-                                  );
-                                },
                               ),
-                            )
-                            : SizedBox(),
-                  ),
-                  SizedBox(height: 2.h),
-                  Center(
-                    child:
-                        movieDetails.runtime != null
-                            ? Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Estreno: ${Conversors.convertDateToString(movieDetails.releaseDate!)}',
-                                  style: GoogleFonts.lora(
-                                    fontSize: 12.sp,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                                SizedBox(width: 2.w),
-                                Text(
-                                  'Duración: ${movieDetails.runtime}',
-                                  style: GoogleFonts.lora(
-                                    fontSize: 12.sp,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                              ],
-                            )
-                            : Text('Duración: Unkown'),
-                  ),
-                  SizedBox(height: 2.h),
-                  Center(
-                    child:
-                        movieDetails.overview != null
-                            ? Text(
-                              '${movieDetails.overview}',
-                              style: GoogleFonts.lora(
-                                fontSize: 12.sp,
-                                fontWeight: FontWeight.w500,
-                              ),
-                              textAlign: TextAlign.center,
-                            )
-                            : SizedBox(),
-                  ),
-                  SizedBox(height: 2.h),
-                  Center(
-                    child:
-                        movieDetails.voteAverage != null
-                            ? StarRating(
-                              rating: movieDetails.voteAverage!,
-                              starSize: 22.sp,
-                            )
-                            : Text('Rating: Unkown'),
-                  ),
-                  SizedBox(height: 2.h),
-                  SizedBox(
-                    height: 200,
-                    child: FutureBuilder(
-                      future: MoviesRepository().getMovieCast(widget.movieId),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return Center(child: CircularProgressIndicator());
-                        }
-                        if (snapshot.hasError) {
-                          return Text(snapshot.error.toString());
-                        }
-                        if (snapshot.hasData) {
-                          final cast = snapshot.data!;
-                          return SizedBox(
-                            width: double.infinity,
-                            child: ListView.separated(
-                              itemCount: cast.length,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: (context, index) {
-                                return CastCard(cast: cast[index]);
-                              },
-                              separatorBuilder:
-                                  (context, index) => SizedBox(width: 2.w),
-                            ),
-                          );
-                        } else {
-                          return Center(child: Text('No hay datos de casting'));
-                        }
-                      },
+                            );
+                          } else {
+                            return Center(
+                              child: Text('No hay datos de casting'),
+                            );
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             );
           } else {
--- lib/pages/tvseries/tv_serie_details.dart
+++ lib/pages/tvseries/tv_serie_details.dart
@@ -32,129 +32,131 @@ class _TvSerieDetailsState extends State<TvSerieDetails> {
           if (snapshot.hasData) {
             final tvSerieDetails = snapshot.data!;
             return Scaffold(
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Text(
-                      tvSerieDetails.name!,
-                      style: GoogleFonts.lora(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+              body: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Text(
+                        tvSerieDetails.name!,
+                        style: GoogleFonts.lora(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 2.h),
-                  Center(
-                    child: Image.network(
-                      height: 35.h,
-                      '${dotenv.env['IMG_URL']}${tvSerieDetails.posterPath}',
-                      fit: BoxFit.cover,
+                    SizedBox(height: 2.h),
+                    Center(
+                      child: Image.network(
+                        height: 35.h,
+                        '${dotenv.env['IMG_URL']}${tvSerieDetails.posterPath}',
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 2.h),
-                  Center(
-                    child:
-                        tvSerieDetails.productionCompanies != null
-                            ? SizedBox(
-                              width: 80.w,
-                              height: 4.h,
+                    SizedBox(height: 2.h),
+                    Center(
+                      child:
+                          tvSerieDetails.productionCompanies != null
+                              ? SizedBox(
+                                width: 80.w,
+                                height: 4.h,
+                                child: ListView.separated(
+                                  scrollDirection: Axis.horizontal,
+                                  separatorBuilder:
+                                      (context, index) => SizedBox(width: 2.w),
+                                  itemCount:
+                                      tvSerieDetails.productionCompanies!.length,
+                                  itemBuilder: (context, index) {
+                                    return Image.network(
+                                      '${dotenv.env['IMG_URL']}${tvSerieDetails.productionCompanies![index].logoPath}',
+                                      errorBuilder:
+                                          (context, error, stackTrace) =>
+                                              Icon(Icons.error, size: 4.h),
+                                    );
+                                  },
+                                ),
+                              )
+                              : SizedBox(),
+                    ),
+                    SizedBox(height: 2.h),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'First Air Date: ${Conversors.convertDateToString(tvSerieDetails.firstAirDate!)}',
+                            style: GoogleFonts.lora(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(width: 2.w),
+                          Text(
+                            'Last Air Date: ${Conversors.convertDateToString(tvSerieDetails.lastAirDate!)}',
+                            style: GoogleFonts.lora(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 2.h),
+                    Center(
+                      child: Text(
+                        '${tvSerieDetails.overview}',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.lora(
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 2.h),
+                    Center(
+                      child: StarRating(
+                        rating: tvSerieDetails.voteAverage!,
+                        starSize: 22.sp,
+                      ),
+                    ),
+                    SizedBox(height: 2.h),
+                    SizedBox(
+                      height: 200,
+                      child: FutureBuilder(
+                        future: SeriesRepository().getTvSerieCast(
+                          tvSerieDetails.id!,
+                        ),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(child: CircularProgressIndicator());
+                          }
+                          if (snapshot.hasError) {
+                            return Text(snapshot.error.toString());
+                          }
+                          if (snapshot.hasData) {
+                            final List<Cast> cast = snapshot.data!;
+                            return SizedBox(
+                              width: double.infinity,
                               child: ListView.separated(
+                                itemCount: cast.length,
                                 scrollDirection: Axis.horizontal,
+                                itemBuilder: (context, index) {
+                                  return CastCard(cast: cast[index]);
+                                },
                                 separatorBuilder:
                                     (context, index) => SizedBox(width: 2.w),
-                                itemCount:
-                                    tvSerieDetails.productionCompanies!.length,
-                                itemBuilder: (context, index) {
-                                  return Image.network(
-                                    '${dotenv.env['IMG_URL']}${tvSerieDetails.productionCompanies![index].logoPath}',
-                                    errorBuilder:
-                                        (context, error, stackTrace) =>
-                                            Icon(Icons.error, size: 4.h),
-                                  );
-                                },
                               ),
-                            )
-                            : SizedBox(),
-                  ),
-                  SizedBox(height: 2.h),
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'First Air Date: ${Conversors.convertDateToString(tvSerieDetails.firstAirDate!)}',
-                          style: GoogleFonts.lora(
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        SizedBox(width: 2.w),
-                        Text(
-                          'Last Air Date: ${Conversors.convertDateToString(tvSerieDetails.lastAirDate!)}',
-                          style: GoogleFonts.lora(
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 2.h),
-                  Center(
-                    child: Text(
-                      '${tvSerieDetails.overview}',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.lora(
-                        fontSize: 12.sp,
-                        fontWeight: FontWeight.w500,
+                            );
+                          } else {
+                            return Text('No data');
+                          }
+                        },
                       ),
                     ),
-                  ),
-                  SizedBox(height: 2.h),
-                  Center(
-                    child: StarRating(
-                      rating: tvSerieDetails.voteAverage!,
-                      starSize: 22.sp,
-                    ),
-                  ),
-                  SizedBox(height: 2.h),
-                  SizedBox(
-                    height: 200,
-                    child: FutureBuilder(
-                      future: SeriesRepository().getTvSerieCast(
-                        tvSerieDetails.id!,
-                      ),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return Center(child: CircularProgressIndicator());
-                        }
-                        if (snapshot.hasError) {
-                          return Text(snapshot.error.toString());
-                        }
-                        if (snapshot.hasData) {
-                          final List<Cast> cast = snapshot.data!;
-                          return SizedBox(
-                            width: double.infinity,
-                            child: ListView.separated(
-                              itemCount: cast.length,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: (context, index) {
-                                return CastCard(cast: cast[index]);
-                              },
-                              separatorBuilder:
-                                  (context, index) => SizedBox(width: 2.w),
-                            ),
-                          );
-                        } else {
-                          return Text('No data');
-                        }
-                      },
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             );
           } else {
